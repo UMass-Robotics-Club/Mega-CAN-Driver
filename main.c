9f39327a5d4120c1a5c65351da823d3d92b2e610
om patel <ogpatel@umass.edu>
2026-03-23 18:43:27 -0400

    added base project

---
+++ main.c
@@ -0,0 +1,11 @@
+/**
+ * @file main.c
+ * @author omp
+ * @date 2026-03-21
+ * @brief Main function
+ */
+int main(){
+    // Add your code here and press Ctrl + Shift + B to build
+
+    return 0;
+}
