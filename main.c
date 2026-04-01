680f73fcd53edb736b9d0738205d78954632a880
Siddharth <110020182+lxremipsxm@users.noreply.github.com>
2026-04-01 19:00:50 -0400

    blink scipt

--- main.c
+++ main.c
@@ -4,8 +4,38 @@
  * @date 2026-03-21
  * @brief Main function
  */
+
+#include <xc.h>
+#include <stdint.h>
+
+void delay(volatile uint32_t count)
+{
+    while (count--)
+    {
+        __asm__ volatile ("nop");
+    }
+}
+
 int main(){
     // Add your code here and press Ctrl + Shift + B to build
+    
+    //blimk scritp
+    WDTCONCLR = _WDTCON_ON_MASK;
+    
+    // Set RB0 as output
+    TRISBCLR = (1 << 0);
+
+    // Initialize LOW
+    LATBCLR = (1 << 0);
+
+    while (1)
+    {
+        // Toggle RB0
+        LATBINV = (1 << 0);
+
+        // Delay
+        delay(500000);
+    }
 
     return 0;
 }
